aa81e7754ca5420bced055302e332d0baa0adb01
Martin Gandeliani <martingandelyan@gmail.com>
2025-10-29 18:37:22 +0400

    Initial Commit

---
+++ 12_ Autolayouts_Martin_Gandeliani/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  12_ Autolayouts_Martin_Gandeliani
+//
+//  Created by Martin on 29.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
