f8203c6bf0b2e257316b3a050d76387df74c924e
Martin Gandeliani <martingandelyan@gmail.com>
2025-11-03 05:23:10 +0400

    updated

---
+++ 12_ Autolayouts_Martin_Gandeliani/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  12_ Autolayouts_Martin_Gandeliani
+//
+//  Created by Martin on 29.10.25.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ 12_ Autolayouts_Martin_Gandeliani/ThirdViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdViewController.swift
+//  12_ Autolayouts_Martin_Gandeliani
+//
+//  Created by Martin on 31.10.25.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
